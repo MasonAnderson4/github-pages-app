162bde95ebdd03e3c2f018637575ea37d22be648
MasonAnderson4 <masonator0465@gmail.com>
2025-10-21 12:37:54 -0500

    added link

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// link to pages: https://masonanderson4.github.io/github-pages-app/
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
